0b63f8ee981479af1cffead1e7a0165ce8fc145f
Mayank Mahajan <mayankm2089@gmail.com>
2023-08-08 23:25:32 +0530

    Updated star-ship installation in server_setup.sh

--- setup_server.sh
+++ setup_server.sh
@@ -5,9 +5,28 @@
 ln -sf $PWD/.bashrc_server ~/.bashrc_server
 
 
-# starship
-curl -sS https://starship.rs/install.sh | sh
+# Starship
+# ------------------------------------------------------------------
+
+# Install nerd-font
+echo "installling nerd-font"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraCode.zip
+mv FiraCode.zip /tmp
+unzip /tmp/FiraCode.zipunzip /tmp/FiraCode.zip -d /tmp/FiraCode
+mkdir ~/.fonts -p
+mv /tmp/FiraCode*.ttf ~/.fonts
+
+echo "installling starship"
+curl -s https://starship.rs/install.sh | sh
+
+# setup starship config
+
 ln -sf $PWD/starship.toml ~/.config/starship.toml
+
+
+# bashrc
+# ------------------------------------------------------------------
+
 
 echo "source ~/.bashrc_server" >> ~/.bashrc
 source ~/.bashrc
