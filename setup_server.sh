5ad40ea44bebe9b8bfeee7d7f89747c22f2235b3
Mayank Mahajan <mayankm2089@gmail.com>
2023-08-08 23:07:18 +0530

    Updated server_setup.sh

--- setup_server.sh
+++ setup_server.sh
@@ -3,7 +3,7 @@
 
 # link .bashrc_server
 ln -sf $PWD/.bashrc_server ~/.bashrc_server
-
+echo "source ~/.bashrc_remote" >> ~/.bashrc
 
 # link starship.toml
 ln -sf $PWD/starship.toml ~/.config/starship.toml
