1814ee4da8f079806ce091f9ba65b66c9c0eefc2
Mayank Mahajan <mayankm2089@gmail.com>
2023-08-08 23:51:15 +0530

    Added setup_django_server.sh

---
+++ setup_django_server.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+set -eu
+
+echo "instaling python3.11"
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install python3.11
+
+echo "installing virtualenv"
+sudo apt install python3-pip
+pip3 install virtualenv
