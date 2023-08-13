ffff1fcbfcee09f89b6f981c5f5b2a24aa2de87a
Mayank Mahajan <mayankm2089@gmail.com>
2023-08-13 14:51:00 +0530

    Added env vars to .bashrc_server.sh

--- config/.bashrc_server.sh
+++ config/.bashrc_server.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 
+# TERMINAL ----------------------------------------------------------
 
-# TERMINAL ---------------------------------------------------------
-
+source .env
 
 #ls
 alias l='ls -CF'
@@ -16,12 +17,12 @@ alias less="less -R"
 export GIT_EDITOR=vim
 
 #HSTR
-alias hh=hstr                    
+alias hh=hstr
 export HSTR_CONFIG=hicolor,no-confirm
-shopt -s histappend              # append new history items to .bash_history
-export HISTCONTROL=ignorespace   # leading space hides commands from history
-export HISTFILESIZE=-1	        # increase history file size (default is 500)
-export HISTSIZE=${HISTFILESIZE}  # increase history size (default is 500)
+shopt -s histappend             # append new history items to .bash_history
+export HISTCONTROL=ignorespace  # leading space hides commands from history
+export HISTFILESIZE=-1          # increase history file size (default is 500)
+export HISTSIZE=${HISTFILESIZE} # increase history size (default is 500)
 # ensure synchronization between bash memory and history file
 export PROMPT_COMMAND="history -a; history -n; ${PROMPT_COMMAND}"
 # if this is interactive shell, then bind hstr to Ctrl-r (for Vi mode check doc)
@@ -30,19 +31,16 @@ if [[ $- =~ .*i.* ]]; then bind '"\C-r": "\C-a hstr -- \C-j"'; fi
 if [[ $- =~ .*i.* ]]; then bind '"\C-xk": "\C-a hstr -k \C-j"'; fi
 
 # autojump
-source  /usr/share/autojump/autojump.sh 
+source /usr/share/autojump/autojump.sh
 alias jl="j --s | fzf"
 
 # Starship
 eval "$(starship init bash)"
 export STARSHIP_CONFIG="$HOME/.config/starship.toml"
 
-
 #ssh
 alias ssh-config="vim ~/.ssh/config"
 alias sc="ssh-config"
-
-
 
 # alias p=pwd
 alias c=clear
@@ -55,8 +53,8 @@ alias w=which
 alias x=exit
 alias s=sudo
 
-alias less="less -R" 			# always display colors
-alias tree="tree -C"			# always display colors
+alias less="less -R" # always display colors
+alias tree="tree -C" # always display colors
 
 # bashrc
 alias bashrc_reload="source ~/.bashrc"
@@ -67,16 +65,29 @@ alias bashrc_edit="vim ~/.bashrc"
 # open a random file in vim
 alias tvim="mktemp | vim -"
 
-# PYTHON ---------------------------------------------------------
+# PYTHON ------------------------------------------------------------
 
 alias p="python"
 alias va="source venv/bin/activate"
 alias da="deactivate"
 
+# Environment Variables----------------------------------------------
+
+# GENERAL
+export APP_GIT_REPO_SSH_URL=""
+export APP_ALIAS="General-App"
+
+export SERVER_NAME="Demo-Env"
+
+# POSTGRES
+
+export PG_VERSION="12"
+export PG_ALLOWED_IP_ADDRESS=""
+
+# Path --------------------------------------------------------------
 # set PATH so it includes user's private bin if it exists
-if [ -d "$HOME/.local/bin" ] ; then
-    PATH="$HOME/.local/bin:$PATH"
+if [ -d "$HOME/.local/bin" ]; then
+  PATH="$HOME/.local/bin:$PATH"
 fi
 
-
-echo "~/.bashrc_server loaded...."
+echo "$HOME/.bashrc_server loaded...."
