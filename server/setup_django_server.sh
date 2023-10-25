b3bf94630661c9d552ea3385df30b3687e199a4c
Mayank Mahajan <mayankm2089@gmail.com>
2023-10-25 22:40:18 +0530

    fix: setup_django_server.sh

--- server/setup_django_server.sh
+++ server/setup_django_server.sh
@@ -7,17 +7,17 @@ set -eu
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
 echo "installing python3.10"
-sudo apt-get install python3.10
-sudo apt-get install python3.10-dev
+sudo apt-get install python3.10 -y
+sudo apt-get install python3.10-dev -y
 
 echo "installing python3.11"
-sudo apt-get install python3.11
-sudo apt-get install python3.11-dev
+sudo apt-get install python3.11 -y
+sudo apt-get install python3.11-dev -y
 
-sudo apt-get install libpq-dev
+sudo apt-get install libpq-dev -y
 
 echo "Installing pip"
-sudo apt install python3-pip
+sudo apt install python3-pip -y
 sudo pip install --upgrade pip
 
 echo "Installing virtualenv"
@@ -30,15 +30,15 @@ echo "Installing ncm"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 # install geospatial libraries
-sudo apt-get install binutils libproj-dev gdal-bin
+sudo apt-get install binutils libproj-dev gdal-bin -y
 
 echo "installing virtualenv"
-sudo apt install python3-pip
+sudo apt install python3-pip -y
 pip3 install virtualenv
 
 echo "installing postgres"
-sudo apt install postgis postgresql postgresql-contrib
-sudo systemctl start postgresql.service
+sudo apt install postgis postgresql postgresql-contrib -y
+sudo systemctl start postgresql.service -y
 # sudo -u postgres psql
 
 # install nvm
