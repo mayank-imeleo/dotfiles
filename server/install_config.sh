5b80386c93d3095451ab7cfee0dcb2aa2ecafdf9
Mayank Mahajan <mayankm2089@gmail.com>
2023-12-16 15:50:48 +0530

    Feat:HSTR blacklist and filesize

--- server/install_config.sh
+++ server/install_config.sh
@@ -11,6 +11,9 @@ cp  "$PWD"/config/starship.toml ~/.config/ -v
 echo "Copying .bashrc_servers.sh"
 cp "$PWD"/config/.bashrc_server.sh ~/ -v
 
+echo "Copying .hstr_blacklist"
+cp "$PWD"/config/.hstr_blacklist ~/ -v
+
 cat <<EOF >> ~/.bashrc
 source "$HOME"/.bashrc_server.sh
 EOF
