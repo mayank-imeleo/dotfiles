f53e54f7d76d969e3b67edce509a80bc4b82d135
Mayank Mahajan <mayankm2089@gmail.com>
2023-12-20 19:27:58 +0530

    Feat: gunicorn setup.sh

---
+++ gunicorn/setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -eu
+
+# Setup system services
+# Usage: ./server/setup.sh
+# $ export GUNICORN_PROJECT_DIR="/home/ubuntu/gunicorn-project"
+# $ ./gunicorn/setup.sh
+
+GUNICORN_PROJECT_DIR=${GUNICORN_PROJECT_DIR:-/home/ubuntu/gunicorn-project}
+
+echo "Setting Up gunicorn log files"
+sudo mkdir -p /var/log/gunicorn/
+sudo touch /var/log/gunicorn/access.log
+sudo touch /var/log/gunicorn/error.log
+
+sudo chown ubuntu:www-data /var/log/gunicorn/*.log -R
+
+
+echo "Configuring nginx"
+sudo rm /etc/nginx/sites-enabled/default -f
+sudo cp "$GUNICORN_PROJECT_DIR"/server/gunicorn.nginx /etc/nginx/sites-enabled/
+
+echo "Configuring system daemons"
+sudo cp "$GUNICORN_PROJECT_DIR"/server/gunicorn.socket /etc/systemd/system/
+sudo cp "$GUNICORN_PROJECT_DIR"/server/gunicorn.service /etc/systemd/system/
+
+echo "Enable system services"
+
+sudo systemctl enable gunicorn.socket
+sudo systemctl enable gunicorn.service
+
+echo "Restarting system daemon services"
+server/restart.sh
+
+#crontab server/cron
+
+echo "Server setup complete"
