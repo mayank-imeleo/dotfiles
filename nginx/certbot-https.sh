ca028556fc6870a6438edec1bca6a17484cc5d61
Mayank Mahajan <mayankm2089@gmail.com>
2023-12-20 20:28:50 +0530

    Feat: gunicorn.nginx

--- nginx/certbot-https.sh
+++ nginx/certbot-https.sh
@@ -2,3 +2,8 @@
 
 # automatically updates the nginx conf
 sudo certbot --nginx -d "$NGINX_SERVER_NAME" -d www."$NGINX_SERVER_NAME"
+
+echo "HTTPS certificate setup complete"
+
+echo "Restart nginx service"
+sudo systemctl restart nginx.service
