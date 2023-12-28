a8679a77ee49cf82a447448d9f77004a83d32622
Mayank Mahajan <mayankm2089@gmail.com>
2023-12-28 21:20:34 +0530

    Feat: django-setup.sh

--- gunicorn/django-setup.sh
+++ gunicorn/django-setup.sh
@@ -12,6 +12,7 @@ virtualenv venv -p "$PYTHON_EXECUTABLE" --clear
 source venv/bin/activate
 
 echo "installing python requirements"
+pip install  git+https://github.com/mayank-m2/pyutils --force-reinstall
 pip install -r "requirements/local.txt"
 pip install -r "requirements/production.txt"
 
