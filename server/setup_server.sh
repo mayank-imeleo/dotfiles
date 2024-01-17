22084e00b2332db7c0966b6485309ac2cb2da5b9
Mayank Mahajan <mayankm2089@gmail.com>
2024-01-17 13:21:46 +0530

    feat: setup_server.sh

--- server/setup_server.sh
+++ server/setup_server.sh
@@ -2,46 +2,93 @@
 
 set -eu
 
-sudo apt update
+./server/update_repository.sh
+
+# ==================================================================
+# OPERATING SYSTEM
+
 mkdir -p "$HOME"/.config
-# basic utilities
-# ------------------------------------------------------------------
 
-sudo apt install \
+# Python Dependencies
+sudo apt-get install \
+  libpq-dev libgl1 \
   unzip glances hstr autojump figlet nnn net-tools \
-  lolcat \
-  multitail -y
+  lolcat multitail \
+  -y
+
+# ==================================================================
+# PYTHON
 
 
-# custom binaries
-# ------------------------------------------------------------------
+echo "installing python3.10"
+sudo apt-get install python3.10 -y
+sudo apt-get install python3.10-dev -y
+
+echo "installing python3.11"
+sudo apt-get install python3.11 -y
+sudo apt-get install python3.11-dev -y
+
+echo "Installing pip"
+sudo apt install python3-pip -y
+sudo pip install --upgrade pip
+
+echo "installing virtualenv"
+sudo apt install python3-pip -y
+pip3 install virtualenv
+
+
+# ==================================================================
+# NGINX
+
+echo "Installing virtualenv"
+pip3 install virtualenv
+
+echo "Installing nginx"
+sudo apt-get install nginx
+
+# Install GoAccess
+sudo apt-get install goaccess -y
+
+# certbot
+sudo snap install core; sudo snap refresh core
+sudo apt remove certbot
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+
+
+# ==================================================================
+# POSTGRES
+
+# install geospatial libraries
+sudo apt-get install binutils libproj-dev gdal-bin -y
+
+
+echo "installing postgres"
+sudo apt install postgis postgresql postgresql-contrib -y
+sudo systemctl start postgresql.service -y
+# sudo -u postgres psql
+
+# ==================================================================
+# NODE
+
+echo "Installing nvm"
+git clone https://github.com/nvm-sh/nvm.git /tmp/nvm
+bash /tmp/nvm/install.sh
+
+# ==================================================================
+# OTHERS
 
 echo "Installing custom binaries"
 # install binaries
 ./server/install_bin.sh
 
-
-
-# Starship
-# ------------------------------------------------------------------
-
 # Install nerd-font
-echo "installling nerd-font"
+echo "installing nerd-font"
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraCode.zip
 mv FiraCode.zip /tmp
 unzip /tmp/FiraCode.zip -d /tmp/FiraCode
 mkdir ~/.fonts -p
 mv /tmp/FiraCode/*.ttf ~/.fonts
 
-echo "installling starship"
+echo "installing starship"
 curl -s https://starship.rs/install.sh | sh
-
-
-
-
-# bashrc
-# ------------------------------------------------------------------
-
-
-# echo "source ~/.bashrc_server" >> ~/.bashrc
-#source ~/.bashrc
---
+++ server/update_repository.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eu
+
+# Add GoAccess to repository
+wget -O - https://deb.goaccess.io/gnugpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/goaccess.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/goaccess.gpg arch=$(dpkg --print-architecture)] https://deb.goaccess.io/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/goaccess.list
+
+# Add python3.10 to repository
+sudo add-apt-repository ppa:deadsnakes/ppa
+
+# Repository update
+sudo apt-get update
