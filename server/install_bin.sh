b47ccdb904d41439a00760b3ef0598a4a7bfab26
Mayank Mahajan <mayankm2089@gmail.com>
2024-03-12 17:13:30 +0530

    feat: update service start scripts

--- server/install_bin.sh
+++ server/install_bin.sh
@@ -6,3 +6,6 @@ mkdir ~/.local/bin -p
 
 echo "create symbolic links from ~/.local/bin to ./bin"
 ln -sf  "$PWD"/bin/* "$HOME"/.local/bin
+
+echo "remove broken symbolic links from ~/.local/bin"
+find . -xtype l -delete
