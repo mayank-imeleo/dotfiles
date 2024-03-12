bb64b0006cf6e1857cac6b874ca7e59088dd41f9
Mayank Mahajan <mayankm2089@gmail.com>
2024-03-12 18:05:44 +0530

    feat: .bashrc_server.sh Journalctl

--- django/start-byobu.sh
+++ django/start-byobu.sh
@@ -41,11 +41,10 @@ byobu new-window -t "$SESSION_NAME" -n "gncrn" -c "$HOME/gunicorn-project/"
 echo "creating dotfiles directory window"
 byobu new-window -t "$SESSION_NAME" -n "dtfls" -c "$HOME/dotfiles/"
 
-echo "Remove items from status bar"
-byobu-quiet
 
 echo "Byobu session $SESSION_NAME successfully started"
 
 
 echo "Attaching to Byobu session $SESSION_NAME"
 byobu attach -t django
+byobu-quiet
