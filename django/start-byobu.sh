fa1a662533a8145b20707307fbdb35c186effdeb
Mayank Mahajan <mayankm2089@gmail.com>
2024-03-12 18:01:11 +0530

    feat: .bashrc_server.sh Journalctl

--- django/start-byobu.sh
+++ django/start-byobu.sh
@@ -9,36 +9,36 @@ echo "Starting Byobu session: $SESSION_NAME"
 byobu new-session -d -s "$SESSION_NAME"
 
 # Nginx Access Log
-echo "Creating Nginx Access Log Window"
+echo "creating nginx access log window"
 byobu new-window -t "$SESSION_NAME" -n "ngnx-ac-lg" \
   "sudo multitail /var/log/nginx/access.log -f"
 
 # Nginx Error Log
-echo "Creating Nginx Error Log Window"
+echo "creating nginx error log window"
 byobu new-window -t "$SESSION_NAME" -n "ngnx-er-lg" \
   "sudo multitail /var/log/nginx/error.log -f"
 
 # Gunicorn Error Log
-echo "Creating Gunicorn Window"
+echo "creating gunicorn window"
 byobu new-window -t "$SESSION_NAME" -n "gncrn-er-lg" \
   "sudo multitail /var/log/gunicorn/error.log -f"
 
 # Gunicorn JournalCtl
-echo "Creating Gunicorn JournalCtl Window"
+echo "creating gunicorn journalctl window"
 byobu new-window -t "$SESSION_NAME" -n "gncrn-jrnctl-lg" \
   "sudo journalctl -u gunicorn -f"
 
 # Celery
-echo "Creating Celery Window"
+echo "creating celery window"
 byobu new-window -t "$SESSION_NAME" -n "clry-lg" \
   "sudo multitail /var/log/celery/worker1.log /var/log/celery/beat.log -f"
 
 # Django
-echo "Creating Django Window"
+echo "creating gunicorn directory window"
 byobu new-window -t "$SESSION_NAME" -n "djg" -c "$HOME/gunicorn-project/"
 
 # Dotfiles
-echo "Creating Dotfiles Window"
+echo "creating dotfiles directory window"
 byobu new-window -t "$SESSION_NAME" -n "dtfls" -c "$HOME/dotfiles/"
 
 echo "Byobu session $SESSION_NAME successfully started"
